ec5da0c4a6f99fcc440fe8a76cd40dc61b4a5178
Dixon Guerrero <dixonguerrero1204@gmail.com>
2026-03-22 23:18:15 -0500

    fix: Adding urls in files queried from the image service

--- src/main/java/com/taskify/taskifyApi/application/service/ImageService.java
+++ src/main/java/com/taskify/taskifyApi/application/service/ImageService.java
@@ -2,6 +2,7 @@ package com.taskify.taskifyApi.application.service;
 
 import com.taskify.taskifyApi.application.ports.input.FileServicePort;
 import com.taskify.taskifyApi.application.ports.input.ImageServicePort;
+import com.taskify.taskifyApi.application.ports.output.FileStoragePort;
 import com.taskify.taskifyApi.application.ports.output.ImagePersistencePort;
 import com.taskify.taskifyApi.domain.enums.ImageFormat;
 import com.taskify.taskifyApi.domain.enums.ImageType;
@@ -20,42 +21,63 @@ public class ImageService implements ImageServicePort {
 
     private final ImagePersistencePort persistencePort;
     private final FileServicePort fileServicePort;
+    private final FileStoragePort fileStoragePort;
 
-    private static final long MAX_FILE_SIZE = 512 * 1024; // 500 KB
+    private static final long MAX_FILE_SIZE = 512 * 1024;
 
     @Override
     public Image findById(Long id) {
-        return persistencePort.findById(id)
+        Image image = persistencePort.findById(id)
                 .orElseThrow(ImageNotFoundException::new);
+
+        hydrateImage(image);
+        return image;
     }
 
     @Override
     public List<Image> findAllByType(ImageType type) {
-        return persistencePort.findAllByType(type);
+        List<Image> images = persistencePort.findAllByType(type);
+        images.forEach(this::hydrateImage);
+        return images;
     }
 
     @Override
     public List<Image> findAll() {
-        return persistencePort.findAll();
+        List<Image> images = persistencePort.findAll();
+        images.forEach(this::hydrateImage);
+        return images;
     }
 
     @Override
-    public Image save(Image image, MultipartFile file, Long  ownerId) {
+    public Image save(Image image, MultipartFile file, Long ownerId) {
         this.validationsImage(file);
+
         File uploadedFile = fileServicePort.save(file, ownerId);
 
         image.setFile(uploadedFile);
+        Image savedImage = persistencePort.save(image);
 
-        return persistencePort.save(image);
+        savedImage.getFile().setUrl(uploadedFile.getUrl());
+
+        return savedImage;
     }
 
     @Override
     public void deleteByID(Long id) {
         Image imageStored = this.findById(id);
-
-        fileServicePort.deleteById(imageStored.getFile().getId());
-
         persistencePort.deleteByID(id);
+        fileServicePort.deleteById(imageStored.getFile().getId());
+    }
+
+    private void hydrateImage(Image image) {
+        if (image != null && image.getFile() != null) {
+            try {
+                String url = fileStoragePort.getFileUrl(image.getFile().getStorageKey());
+                image.getFile().setUrl(url);
+            } catch (Exception e) {
+                image.getFile().setUrl(null);
+            }
+        }
     }
 
     private void validationsImage(MultipartFile file) {
