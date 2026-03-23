390be88bd550520f0c07f08b1a269fb94c9ed280
Dixon Guerrero <dixonguerrero1204@gmail.com>
2026-03-22 21:43:53 -0500

    fix: Message was not displayed correctly when trying to access a resource without authorization

--- src/main/java/com/taskify/taskifyApi/infrastructure/input/config/security/filter/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/taskify/taskifyApi/infrastructure/input/config/security/filter/CustomAuthenticationEntryPoint.java
@@ -1,17 +1,40 @@
 package com.taskify.taskifyApi.infrastructure.input.config.security.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.taskify.taskifyApi.infrastructure.input.exception.UnauthorizedException;
+import com.taskify.taskifyApi.infrastructure.input.model.response.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Collections;
 
+@Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException) throws IOException {
-        throw new UnauthorizedException("Authentication failed: " + authException.getMessage());
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        ErrorResponse errorBody = ErrorResponse.builder()
+                .code("AUTH_UNAUTHORIZED")
+                .message("Unauthorized access")
+                .details(Collections.singletonList(authException.getMessage()))
+                .timestamp(LocalDateTime.now())
+                .build();
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new com.fasterxml.jackson.datatype.jsr310.JavaTimeModule());
+        mapper.disable(com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        response.getWriter().write(mapper.writeValueAsString(errorBody));
     }
 }
