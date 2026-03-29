223c46a963f7c226bf010bb916bd861950dd5826
Dixon Guerrero <dixonguerrero1204@gmail.com>
2026-03-29 14:10:27 -0500

    feat: Adjusting so that the token and image duration is per variable

---
+++ src/main/java/com/taskify/taskifyApi/infrastructure/config/AppProperties.java
@@ -0,0 +1,28 @@
+package com.taskify.taskifyApi.infrastructure.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "app")
+public class AppProperties {
+
+    private final Cookie cookie = new Cookie();
+    private final Security security = new Security();
+
+    @Getter
+    @Setter
+    public static class Cookie {
+        private boolean secure;
+    }
+
+    @Getter
+    @Setter
+    public static class Security {
+        private Long jwtExpirationMs;
+    }
+}
--- src/main/java/com/taskify/taskifyApi/infrastructure/output/adapters/MinioStorageAdapter.java
+++ src/main/java/com/taskify/taskifyApi/infrastructure/output/adapters/MinioStorageAdapter.java
@@ -8,6 +8,7 @@ import io.minio.errors.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -82,7 +83,7 @@ public class MinioStorageAdapter implements FileStoragePort {
                         .method(io.minio.http.Method.GET)
                         .bucket(minioProperties.getBucket())
                         .object(fileName)
-                        .expiry(60 * 60)
+                        .expiry(minioProperties.getUrlExpirationSeconds())
                         .build()
         );
     }
